4359b747e99da3d9813bba25c5d78e9d1a672c3c
Gio <glandaburo92@gmail.com>
2022-10-26 11:21:24 +0200

    add folders to MVCV

---
+++ project_final/src/pages/tasks/TasksPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import TaskListComponent from "../../components/containers/task_list";
+
+const TasksPage = () => {
+  return (
+    <div>
+      <h1>
+        <TaskListComponent></TaskListComponent>
+      </h1>
+    </div>
+  );
+};
+
+export default TasksPage;
