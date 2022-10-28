48bf87d3c5602f9065eda7c660aa8fbf02ef72b1
Gio <glandaburo92@gmail.com>
2022-10-28 13:06:16 +0200

    add login Form with formik and yup

--- project_final/src/App.js
+++ project_final/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import TaskListComponent from './components/containers/task_list';
+import Loginform from './components/pure/forms/loginForm';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <TaskListComponent></TaskListComponent>
+        {/* <TaskListComponent></TaskListComponent> */}
+        <Loginform></Loginform>
       </header>
     </div>
   );
