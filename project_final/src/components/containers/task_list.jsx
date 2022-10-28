b7b4d22066904201b6fb7c80b70aaa23d087206b
Gio <glandaburo92@gmail.com>
2022-10-28 11:10:32 +0200

    fix delete tasks and add underline to pending task

--- project_final/src/components/containers/task_list.jsx
+++ project_final/src/components/containers/task_list.jsx
@@ -5,96 +5,117 @@ import TaskComponent from "../pure/forms/task";
 import TaskForm from "../pure/forms/taskForm";
 
 const TaskListComponent = () => {
-  const defaultTask = new Task(
-    "First 1",
-    "aquí se ejecutará una tarea",
-    false,
-    LEVELS.NORMAL
-  );
-  const defaultTask2 = new Task(
-    "First 2",
-    "aquí se ejecutará una tarea Urgente",
-    true,
-    LEVELS.URGENT
-  );
-  const defaultTask3 = new Task(
-    "First 3",
-    "aquí se ejecutará una tarea bloaquead",
-    false,
-    LEVELS.BLOCKING
-  );
 
-  const [tasks, setTask] = useState([defaultTask, defaultTask2, defaultTask3]);
+  const defaultTask1 = new Task('Example1', 'Description1', true, LEVELS.NORMAL);
+  const defaultTask2 = new Task('Example2', 'Description 2', false, LEVELS.URGENT);
+  const defaultTask3 = new Task('Example3', 'Description 3', false, LEVELS.BLOCKING);
+
+
+  // Estado del componente
+  const [tasks, setTasks] = useState([defaultTask1, defaultTask2, defaultTask3]);
   const [loading, setLoading] = useState(true);
 
+  // Control del ciclo de vida del componente
   useEffect(() => {
-    setLoading(false);
-    return () => {
-      console.log(`unmount ${tasks}`);
-    };
-  }, [tasks]);
+      console.log('Task State has been modified');
+      setTimeout(() => {
+          setLoading(false);
+      }, 2000);
+      return () => {
+          console.log('TaskList component is going to unmount...')
+      }
+  }, [tasks])
 
-  function completeTask(task) {
-    console.log(`completed ${task}`);
-    const index = tasks.indexOf(task);
-    const tempTask = [...tasks];
-    tempTask[index].completed = !tempTask[index].completed;
-    setTask(tempTask);
+
+  function completeTask(task){
+      const index = tasks.indexOf(task);
+      const tempTasks = [...tasks];
+      tempTasks[index].completed = !tempTasks[index].completed;
+      setTasks(tempTasks);
   }
 
-  function deleteTask(task) {
-    console.log(`deleted ${task}`);
-    const index = tasks.indexOf(task);
-    const tempTask = [...tasks];
-    tempTask.slice(index, 1);
-    setTask(tempTask);
+  function deleteTask(task){
+      const index = tasks.indexOf(task);
+      const tempTasks = [...tasks];
+      tempTasks.splice(index,1);
+      setTasks(tempTasks);
   }
 
-  function addTask(task) {
-    console.log(`create ${task}`);
-    const tempTask = [...tasks];
-    tempTask.push(task);
-    setTask(tempTask);
+  function addTask(task){
+      const tempTasks = [...tasks];
+      tempTasks.push(task);
+      setTasks(tempTasks);
+  }
+
+  const Table = () => {
+      return (
+          <table>
+              <thead>
+                  <tr>
+                      <th scope='col'>Title</th>
+                      <th scope='col'>Description</th>
+                      <th scope='col'>Priority</th>
+                      <th scope='col'>Actions</th>
+                  </tr>
+              </thead>
+              <tbody>
+                  { tasks.map((task, index) => {
+                      return (
+                              <TaskComponent 
+                                  key={index} 
+                                  task={task}
+                                  complete={completeTask}
+                                  remove = {deleteTask}
+                              >
+                              </TaskComponent>
+                          )
+                      }
+                  )}
+              </tbody>
+          </table>
+      )
+  }
+
+  let tasksTable;
+
+  if(tasks.length > 0){
+      tasksTable = <Table></Table>
+  }else{
+      tasksTable = (
+      <div>
+          <h3> There are no tasks to show</h3>
+          <h4>Please, create one</h4>
+      </div>
+      )
+  }
+
+  const loadingStyle = {
+      color: 'grey',
+      fontSize: '30px',
+      fontWeight: 'bold'
   }
 
   return (
-    <div className="col-12">
-      <div className="card">
-        <div className="card-header p-3">
-          <h5>Your Tasks:</h5>
-        </div>
-        <div
-          className="card-body"
-          style={{ position: "relative", height: "400px", color: "black" }}
-          data-mdb-perfect-scrollbar="true"
-        >
-          <table>
-            <thead>
-              <tr>
-                <th scope="col">Title</th>
-                <th scope="col">Description</th>
-                <th scope="col">Priority</th>
-                <th scope="col">Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {tasks.map((task, index) => {
-                return (
-                  <TaskComponent
-                    key={index}
-                    task={task}
-                    complete={completeTask}
-                     remove={deleteTask}
-                  ></TaskComponent>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
+      <div>
+          <div className='col-12'>
+              <div className='card'>
+                  {/* Card Header (title) */}
+                  <div className='card-header p-3'>
+                      <h5>
+                          Your Tasks:
+                      </h5>
+                  </div>
+                  {/* Card Body (content) */}
+                  <div className='card-body' data-mdb-perfect-scrollbar='true' style={ {position: 'relative', height: '400px'} }>
+                      {/* TODO: Add Loading Spinner */}
+                      {loading ? (<p style={loadingStyle}>Loading tasks...</p>) : tasksTable}
+                  </div>
+              </div>
+          </div>
+          <TaskForm add={addTask} length={tasks.length}></TaskForm>
       </div>
-        <TaskForm add={addTask}></TaskForm>
-    </div>
   );
 };
+
 
 export default TaskListComponent;
--- project_final/src/components/pure/forms/task.jsx
+++ project_final/src/components/pure/forms/task.jsx
@@ -5,85 +5,86 @@ import { Task } from "../../../models/task.class";
 import { LEVELS } from "../../../models/levels.enum";
 
 const TaskComponent = ({ task, complete, remove }) => {
+
   useEffect(() => {
-    console.log("Created task");
-    return () => {
-      console.log(`unmount ${task}`);
-    };
+      console.log('Created Task')
+      return () => {
+          console.log(`Task: ${task.name} is going to unmount`);
+      }
   }, [task]);
 
-  function taskLevelBadge() {
-    switch (task.level) {
-      case LEVELS.NORMAL:
-        return (
-          <h6 className="mb-0">
-            <span className="badge bg-primary">{task.level}</span>
-          </h6>
-        );
-      case LEVELS.URGENT:
-        return (
-          <h6 className="mb-0">
-            <span className="badge bg-warning">{task.level}</span>
-          </h6>
-        );
-      case LEVELS.BLOCKING:
-        return (
-          <h6 className="mb-0">
-            <span className="badge bg-danger">{task.level}</span>
-          </h6>
-        );
-      default:
-        break;
-    }
+  function taskLevelBadge(){
+      switch (task.level) {
+          case LEVELS.NORMAL:
+              return(
+              <h6 className='mb-0'>
+                  <span className='badge bg-primary'>
+                      {task.level}
+                  </span>
+              </h6>)
+          case LEVELS.URGENT:
+              return(
+              <h6 className='mb-0'>
+                  <span className='badge bg-warning'>
+                      {task.level}
+                  </span>
+              </h6>)
+          case LEVELS.BLOCKING:
+              return(
+              <h6 className='mb-0'>
+                  <span className='badge bg-danger'>
+                      {task.level}
+                  </span>
+              </h6>)
+          default:
+              break;
+      }
   }
-  function taskIconsCompleted() {
-    if (task.completed) {
-      return (
-        <i
-          onClick={() => complete(task)}
-          className="bi-toggle-on task-action"
-          style={{ color: "green" }}
-        ></i>
-      );
-    } else {
-      return (
-        <i
-          onClick={() => complete(task)}
-          className="bi-toggle-off task-action"
-          style={{ color: "gray" }}
-        ></i>
-      );
-    }
+
+  function taskCompletedIcon(){
+      if(task.completed){
+          return (<i onClick={() => complete(task)} className='bi-toggle-on task-action' style={{color: 'green'}}></i>)
+      }else{
+          return (<i onClick={() => complete(task)} className='bi-toggle-off task-action' style={{color: 'grey'}}></i>)
+      }
+  }
+
+  const taskCompleted = {
+      color: 'gray',
+      fontWeight: 'bold',
+      textDecoration: 'line-through'
+  }
+
+  const taskPending = {
+      fontWeight: 'bold',
+      color: 'tomato'
   }
 
   return (
-    <tr className="fw-normal">
-      <th>
-        <span className="ms-2">{task.name} </span>
-      </th>
-      <td className="align-middle">
-        <span>{task.description} </span>
-      </td>
-      <td className="align-middle">
-        {taskLevelBadge()}
-        <td className="align-middle">
-          {taskIconsCompleted()}
-          <i
-            className="bi-trash task-action"
-            style={{ color: "gray" }}
-            onClick={() => remove(task)}
-          ></i>
-          <span>{task.completed ? "Completed" : "Pending"} </span>
-        </td>
-      </td>
-    </tr>
+      <tr className='fw-normal' style={task.completed ? taskCompleted : taskPending}>
+          <th>
+              <span className='ms-2'>{task.name}</span>
+          </th>
+          <td className='align-middle'>
+              <span>{task.description}</span>
+          </td>
+          <td className='align-middle'>
+              {taskLevelBadge()}
+          </td>
+          <td className='align-middle'>
+              {taskCompletedIcon()}
+              <i className='bi-trash task-action' style={{color: 'tomato'}} onClick={() => remove(task)}></i>
+          </td>
+      </tr>
   );
 };
+
 
 TaskComponent.propTypes = {
   task: PropTypes.instanceOf(Task).isRequired,
   complete: PropTypes.func.isRequired,
-  remove: PropTypes.func.isRequired,
+  remove: PropTypes.func.isRequired
 };
+
 
 export default TaskComponent;
