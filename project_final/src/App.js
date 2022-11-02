8c6424d8ea49da73f8312dd42a42768079d5d7c1
Gio <glandaburo92@gmail.com>
2022-11-02 11:33:30 +0100

    add register form

--- project_final/src/App.js
+++ project_final/src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import TaskListComponent from './components/containers/task_list';
 import Loginform from './components/pure/forms/loginForm';
+import RegisterForm from './components/pure/forms/registerForm';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         {/* <TaskListComponent></TaskListComponent> */}
-        <Loginform></Loginform>
+        <RegisterForm></RegisterForm>
       </header>
     </div>
   );
--- project_final/src/components/pure/forms/registerForm.jsx
+++ project_final/src/components/pure/forms/registerForm.jsx
@@ -1,23 +1,117 @@
-import React, { useState } from 'react'
-import PropTypes from 'prop-types'
+import React, { useState } from "react";
+import { User } from "../../../models/users.class";
+import { Formik, Field, Form, ErrorMessage, yupToFormErrors } from "formik";
+import * as Yup from "yup";
+import { ROLES } from "../../../models/roles.enum";
 
-const RegisterForm = props => {
+const RegisterForm = (props) => {
+  let user = new User();
 
-    const initialCredentials = [
-        {
-          user: "",
-          name:"",
-          mail:"",
-          pass: ""
-        },
-      ];
-      const [credentials, setCredentials] = useState(initialCredentials);
-     
+  const initialValues = {
+    username: "",
+    email: "",
+    password: "",
+    confirm: "",
+    role: ROLES.USER,
+  };
+  const registerSchema = Yup.object().shape({
+    username: Yup.string()
+      .min(6, "username too short")
+      .max(12, "username too long")
+      .required("username is required"),
+    email: Yup.string()
+      .email("Invalid email format")
+      .required("Email is required"),
+    //   role: Yup.string().oneOf([ROLES.USER, ROLES.ADMIN],'You must select a Role: User/Admin')
+    // .required('Role is requiered'),
+      password: Yup.string()
+      .min(8, "password too short")
+      .required("Password is required"),
+    confirm: Yup.string()
+      .when("password", {
+        is: (value) => (value && value.length > 0 ? true : false),
+        then: Yup.string().oneOf([Yup.ref("password")], "password must match"),
+      })
+      .required("you must confirm the password"),
+  });
+
+  const submit = (values) => {
+    alert("Registered User");
+  };
+
   return (
-    <div>registerForm</div>
-  )
-}
+    <div>
+      <h4>Register form</h4>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={registerSchema}
+        onSubmit={async (values) => {
+          await new Promise((r) => setTimeout(r, 1000));
+          alert(JSON.stringify(values, null, 2));
+        }}
+      >
+ {({ values,
+                    touched,
+                    errors,
+                    isSubmitting,
+                    handleChange,
+                    handleBlur })=>(
+                      <Form>
+                      <label htmlFor="username">Username</label>
+                            <Field id="username" type="text" name="username" placeholder="Your username" />
+                            {
+                                errors.username && touched.username && 
+                                (
+                                    <ErrorMessage name="username" component='div'></ErrorMessage>
+                                )
+                            }
+                      <label htmlFor="email">Email</label>
+                            <Field id="email" type="email" name="email" placeholder="example@email.com" />
+                            {
+                                errors.email && touched.email && 
+                                (
+                                    <ErrorMessage name="email" component='div'></ErrorMessage>
+                                )
+                            }
+                            <label htmlFor="password">Password</label>
+                            <Field
+                                id="password"
+                                name="password"
+                                placeholder="password"
+                                type='password'
+                            />
+                            {/* Password Errors */}
+                            {
+                                errors.password && touched.password && 
+                                (
+                                    <ErrorMessage name="password" component='div'></ErrorMessage>
+                                )
+                            } 
+                            <label htmlFor="confirm">Confirm</label>
+                            <Field
+                                id="confirm"
+                                name="confirm"
+                                placeholder="Confirm your passsword"
+                                type='password'
+                            />
+                            {/* Password Errors */}
+                            {
+                                errors.confirm && touched.confirm && 
+                                (
+                                    <ErrorMessage name="confirm" component='div'></ErrorMessage>
+                                )
+                            }
+                            <button type="submit">Register</button>
+                            {isSubmitting ? (<p>Sending your credentials ...</p>): null}
 
-RegisterForm.propTypes = {}
+                      </Form>
+                    )}
 
-export default RegisterForm
+      </Formik>
+    </div>
+  );
+};
+
+RegisterForm.propTypes = {};
+
+export default RegisterForm;
