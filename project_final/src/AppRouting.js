4d6688905a99cfe1db661ed38421a4012d218e18
Gio <glandaburo92@gmail.com>
2022-11-11 14:56:21 +0100

    add dashboard and mui

--- project_final/src/AppRouting.js
+++ project_final/src/AppRouting.js
@@ -6,6 +6,8 @@ import AboutPage from "./pages/about-faqs/AboutPage";
 import ProfilePage from "./pages/Profile/ProfilePage";
 import TaskListComponent from "./components/containers/task_list";
 import TaskDetailsComponents from "./pages/tasks/TaskDetailsComponents";
+import DashBoard from "./pages/dashboard/DashBoard";
+import Loginform from "./components/pure/forms/loginForm";
 
 function AppRouting() {
   return (
@@ -15,15 +17,16 @@ function AppRouting() {
           <Link to="/">| Home |</Link>
           <Link to="/profile">| Profile |</Link>
           <Link to="/about">| About |</Link>
-          <Link to="/faqs">| FAQS |</Link>
+          <Link to="/Login">| FAQS |</Link>
           <Link to="/faqs">| FAQS |</Link>
           <Link to="/task">| TaskList |</Link>
           
         </aside>
         <main>
           <Routes>
-            <Route path="/" element={<HomePage />} />
+            <Route path="/" element={<DashBoard />} />
             <Route path="/profile"  element={<ProfilePage />} />
+            <Route path="/login"  element={<Loginform />} />
             <Route path="/about"  element={<AboutPage />} />
             <Route path="/faqs"  element={<AboutPage />} />
             <Route path="/task"  element={<TaskListComponent />} />
